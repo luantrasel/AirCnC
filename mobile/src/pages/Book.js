b5ee8116ef741cccb712e7c8eb4dd0fb7eadd11e
unknown <luantrasel77@gmail.com>
2019-10-08 17:17:41 -0300

    adicionado datepicker

--- mobile/src/pages/Book.js
+++ mobile/src/pages/Book.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
 import { SafeAreaView, Alert, Text, StyleSheet, AsyncStorage, TextInput, TouchableOpacity } from 'react-native';
 
+import moment from 'moment';
+import DatePicker from 'react-native-datepicker'
+
 import api from '../services/api';
 
 export default function Book({ navigation }) {
-    const [date, setDate] = useState('');
+    const dateFormat = "DD/MM/YYYY";
+    const currentDate = moment(new Date()).format(dateFormat);
+    const [date, setDate] = useState(currentDate);
     const id = navigation.getParam('id');
 
     async function handleSubmit(){
@@ -26,14 +31,44 @@ export default function Book({ navigation }) {
 
     return (
         <SafeAreaView style={styles.container}> 
-            <Text style={styles.label}>DATA DE INTERESSE *</Text>
-            <TextInput
-                style={styles.input}
-                placeholder='Qual data você deseja reservar?'
-                placeholderTextColor='#999'                
-                autoCorrect={true}
-                value={date}
-                onChangeText={(text => setDate(text))}
+            <Text style={styles.label}>DATA DE INTERESSE *</Text>            
+
+            {/*https://github.com/xgfe/react-native-datepicker*/}
+            <DatePicker
+                date={date}
+                format={dateFormat}
+                showIcon={false}
+                mode="date"            
+                style={styles.datePicker}
+                customStyles={{
+                    dateInput: {                        
+                        borderRadius: 10,
+                        borderWidth: 1,
+                        borderColor: '#ddd',
+                        color: '#444',                        
+                    }
+                }}
+                // sem os textos, fica bugado no iOS
+                confirmBtnText="Confirm"
+                cancelBtnText="Cancel"
+                minDate={currentDate}
+                
+                // placeholder="select date"        
+                // minDate="2016-05-01"
+                // maxDate="2016-06-01"                
+                // customStyles={{
+                //   dateIcon: {
+                //     position: 'absolute',
+                //     left: 0,
+                //     top: 4,
+                //     marginLeft: 0
+                //   },
+                //   dateInput: {
+                //     marginLeft: 36                
+                //   }
+                //   // ... You can check the source to find the other keys.
+                // }}
+                onDateChange={date => setDate(date)}                
             />
 
         <TouchableOpacity onPress={handleSubmit} style={styles.button}>
@@ -75,17 +110,22 @@ const styles = StyleSheet.create({
         backgroundColor: '#f05a5b',
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: 2        
+        borderRadius: 2
     },
 
     cancelButton: {
-        backgroundColor: '#ccc',    
-        marginTop: 10  
+        backgroundColor: '#ccc',
+        marginTop: 10
     },
 
     buttonText: {
         color: '#fff',
         fontWeight: 'bold',
         fontSize: 16
+    },
+
+    datePicker: {
+        marginBottom: 8,
+        width: '100%'
     }
 });
